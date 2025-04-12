235485f77c45450b16a2c4eaac8a825c403ddd08
AnaghaInowei <171633001+AnaghaInowei@users.noreply.github.com>
2025-04-12 04:37:51 +0100

    main

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ document.querySelector('.more').addEventListener('click', function() {
     // Select all blog-content elements that also have the 'hide' class
     const hiddenDivs = document.querySelectorAll('.hide');
     
-    // Remove the 'hide' class from each element to display them
     hiddenDivs.forEach(div => {
-      div.classList.remove('hide');
+    div.classList.remove('hide');
     });
     
-    // Optionally, you can hide the button after displaying the contents
-    // this.style.display = 'none';
-  });
+});
